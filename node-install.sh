04299e2c6d272dc98570a386f86678e8b76871e6
MichaIng <micha@dietpi.com>
2021-05-05 17:52:54 +0200

    Simplify getting unofficial versions + alignments
    Since the index.tab is sorted already, we can simply pick the first match. Merge multiple tools into a single awk call.
    
    Additionally order and outputs have been adjusted to be more reasonable, so that e.g. no version is searched before root permissions have been verified, and no "latest version found" is printed when a specific version was given. Also the doubled download check (HEAD request) for unofficial version has been removed, the actual download will fail instead, printing a meaningful curl error messages, in case.
    
    Lastly the if-then-else blocks have been further aligned to match the most common code style.

--- node-install.sh
+++ node-install.sh
@@ -20,118 +20,105 @@ END
 }
 
 ARCH=$(uname -m)
-UNOFFICIALS_URI=https://unofficial-builds.nodejs.org/download/release
+UNOFFICIALS_URI='https://unofficial-builds.nodejs.org/download/release'
+UNOFFICIAL=0
 
 list_available() {
-    if command -v 'curl' > /dev/null && command -v 'sort' > /dev/null; then
-        if command -v 'tail' > /dev/null && command -v 'cut' > /dev/null; then
-            curl -sSf $UNOFFICIALS_URI/index.tab | grep $ARCH | cut -f1 | tail -n +2 | sort -Vr
-        # elif command -v 'jq' > /dev/null; then
-        #     curl -sSf "$UNOFFICIALS_URI/index.json" | jq -r '.[].version' | sort -Vr
-        else
-            # echo 'ERROR: Required command line tools tail + cut, OR jq not found.' >&2
-            echo 'ERROR: Required command line tools tail + cut not found.' >&2
-            exit 1
-        fi
+    if command -v 'curl' > /dev/null && command -v 'awk' > /dev/null; then
+        curl -sSf "$UNOFFICIALS_URI/index.tab" | awk "/$ARCH/{print \$1}"
     else
-        echo 'ERROR: Required command line tools curl, sort not found.' >&2
+        echo 'ERROR: Required command line tools curl + awk not found.' >&2
         exit 1
     fi
 }
 
-# getting options
+# Getting options
 while [[ $1 ]]; do
     case $1 in
-    -h | --help )
+    '-h'|'--help')
         show_help
         exit
         ;;
-    -lu | --list-unofficial )
+    '-lu'|'--list-unofficial')
         list_available
         exit
         ;;
-    -u | --unofficial )
-        shift; unofficial_build_version=$1
-        if [[ -z $unofficial_build_version ]]; then
-            unofficial_build_version=$(curl -sSf $UNOFFICIALS_URI/index.tab | grep $ARCH | cut -f1 | tail -n +2 | sort -V | tail -n 1)
-            echo "Found latest version $unofficial_build_version for architecture $ARCH."
-        fi
+    '-u'|'--unofficial')
+        UNOFFICIAL=1
+        shift
+        VER=$1
         ;;
-    -- )
+    '--')
         break
         ;;
-    * )
-        echo 'ERROR: Unkown parameter \"$1\" given.' >&2 && exit 1
+    *)
+        echo 'ERROR: Unkown parameter \"$1\" given.' >&2
+        exit 1
+        ;;
     esac
     shift
 done
 
-if [[ $EUID != 0 ]]
-    then
-        echo 'root permissions required for installing Node.js'
-        sudo echo 'sudo permissions verified'
-        exit_status=$?
-        if [[ $exit_status != 0 ]]
-            then
-            echo 'ERROR: Failed to get root permissions via "sudo"' >&2
-            exit $exit_status
-        fi
-    else
-        echo 'root permissions verified'
+if [[ $EUID != 0 ]]; then
+    echo 'root permissions required for installing Node.js'
+    sudo echo 'sudo permissions verified'
+    exit_status=$?
+    if [[ $exit_status != 0 ]]; then
+        echo 'ERROR: Failed to get root permissions via "sudo"' >&2
+        exit $exit_status
+    fi
+else
+    echo 'root permissions verified'
 fi
 
-if [[ $unofficial_build_version ]]; then
-    echo "Searching version $unofficial_build_version for $ARCH ..."
-    URL="$UNOFFICIALS_URI/${unofficial_build_version}/"
-    NAME="node-${unofficial_build_version}-linux-${ARCH}.tar.gz"
-    curl --output /dev/null --silent --head --fail "${URL}${NAME}" || NAME='' # check if file exists
-    NAME="\"$NAME" # add double quote sign to mimic logic from original script block below.
+if [[ $UNOFFICIAL == 1 ]]; then
+    if [[ $VER ]]; then
+        echo "The following unofficial version was requested: $VER"
+    else
+        echo "Searching latest unofficial version for $ARCH ..."
+        VER=$(curl -sSf "$UNOFFICIALS_URI/index.tab" | awk "/$ARCH/{print \$1;exit}")
+        if [[ ! $VER ]]; then
+            echo "ERROR: Failed to find any unofficial version for $ARCH" >&2
+            exit 1
+        fi
+        echo "Found latest unofficial version for $ARCH: $VER"
+    fi
+    URL="$UNOFFICIALS_URI/$VER/node-$VER-linux-$ARCH.tar.gz"
 else
     echo "Searching latest stable version for $ARCH ..."
     URL='https://nodejs.org/dist/'
-    if [[ $ARCH == 'aarch64' ]]
-    then
+    if [[ $ARCH == 'aarch64' ]]; then
         URL+='latest/'
         NAME=$(curl -sSf "$URL" | grep -o '"node-v[0-9.]*-linux-arm64.tar.gz')
-
-    elif [[ $ARCH == 'armv6l' ]]
-    then
+    elif [[ $ARCH == 'armv6l' ]]; then
         URL+='latest-v11.x/'
         NAME=$(curl -sSf "$URL" | grep -o '"node-v[0-9.]*-linux-armv6l.tar.gz')
-
-    elif [[ $ARCH == 'armv7l' ]]
-    then
+    elif [[ $ARCH == 'armv7l' ]]; then
         URL+='latest/'
         NAME=$(curl -sSf "$URL" | grep -o '"node-v[0-9.]*-linux-armv7l.tar.gz')
-
-    elif [[ $ARCH == 'x86_64' ]]
-    then
+    elif [[ $ARCH == 'x86_64' ]]; then
         URL+='latest/'
         NAME=$(curl -sSf "$URL" | grep -o '"node-v[0-9.]*-linux-x64.tar.gz')
-
-    elif [[ $ARCH == 'i'[3-6]'86' ]]
-    then
+    elif [[ $ARCH == 'i'[3-6]'86' ]]; then
         URL+='latest-v9.x/'
         NAME=$(curl -sSf "$URL" | grep -o '"node-v[0-9.]*-linux-x86.tar.gz')
     fi
-fi
-VER=${NAME:1}
-if [[ ! $VER ]]
-    then
-        echo "ERROR: Failed to find latest stable version for $ARCH" >&2
+    VER=${NAME:1}
+    if [[ ! $VER ]]; then
+        echo "ERROR: Failed to find any stable version for $ARCH" >&2
         exit 1
+    fi
+    echo "Found latest stable version for $ARCH: $VER"
+    URL+=$VER
 fi
-echo "Found latest stable version for $ARCH: $VER"
 
-URL+=$VER
 echo "Downloading $URL ..."
 FILE_PATH='/tmp/node.tar.gz'
 curl -fo "$FILE_PATH" "$URL"
 exit_status=$?
-if [[ $exit_status != 0 ]]
-    then
-        echo "ERROR: Failed to download $URL" >&2
-        exit $exit_status
+if [[ $exit_status != 0 ]]; then
+    echo "ERROR: Failed to download $URL" >&2
+    exit $exit_status
 fi
 echo 'Finished downloading!'
 
@@ -139,10 +126,9 @@ echo "Installing $FILE_PATH ..."
 [[ -d '/usr/local' ]] || sudo mkdir /usr/local
 cd /usr/local && sudo tar --strip-components=1 -xzf "$FILE_PATH"
 exit_status=$?
-if [[ $exit_status != 0 ]]
-    then
-        echo "ERROR: Failed to extract $FILE_PATH" >&2
-        exit $exit_status
+if [[ $exit_status != 0 ]]; then
+    echo "ERROR: Failed to extract $FILE_PATH" >&2
+    exit $exit_status
 fi
 rm -v "$FILE_PATH"
 echo 'Finished installing!'
